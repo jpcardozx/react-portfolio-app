3d3696d99e4961f4033c00b0e9d4e12441ef8b7a
jpcardozx <jpcardozoo0106@gmail.com>
2024-09-17 22:18:48 -0300

    deploy json fix

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -8,17 +8,25 @@ interface ProjectProps {
     title: string;
     description: string;
     imageUrl: string;
+    linkUrl: string;
   };
 }
 
 const ProjectCard: React.FC<ProjectProps> = ({ project }) => {
+  const handleButtonClick = () => {
+    window.open(project.linkUrl, '_blank', 'noopener,noreferrer');
+  };
+
   return (
     <div className="project-card">
       <img src={project.imageUrl} alt={project.title} className="project-image" />
       <div className="project-info">
         <h3>{project.title}</h3>
         <p>{project.description}</p>
-        <Link to={`/project/${project.id}`} className="view-more">View More</Link>
+        {/* Link interno para uma página de detalhes do projeto */}
+        <Link to={`/project/${project.id}`} className="view-more">Ver mais</Link>
+        {/* Botão para o link externo */}
+        <button className="view-more" onClick={handleButtonClick}>Ver Projeto</button>
       </div>
     </div>
   );
--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -5,11 +5,36 @@ import '/src/styles/ProjectList.css';
 const ProjectCard = React.lazy(() => import('./ProjectCard'));
 
 const projects = [
-  { id: 1, title: "Social Links Profile", description: "An intuitive profile page designed to centralize your essential social media links in one responsive, easy-to-navigate space. Perfect for enhancing your online presence and boosting engagement.", imageUrl: "https://github.com/jpcardozx/react-portfolio-app/blob/main/src/assets/social-media-links.png?raw=true" },
-  { id: 2, title: "Product Ad | UI/UX Design", description: "A highly optimized product ad landing page, built with a focus on user engagement and lead conversion. This design combines modern UI/UX principles for a smooth and effective experience.", imageUrl: "https://github.com/jpcardozx/react-portfolio-app/blob/main/src/assets/ad-prvw.png?raw=true" },
-  { id: 3, title: "Testimonials Grid | Advanced CSS", description: "A sleek, responsive grid showcasing client testimonials using advanced CSS. This layout enhances credibility and fits seamlessly across all screen sizes, perfect for businesses highlighting social proof.", imageUrl: "https://github.com/jpcardozx/react-portfolio-app/blob/main/src/assets/tgrid.png?raw=true" },
-  { id: 4, title: "Blog Preview Card | CSS Flexbox", description: "A customizable blog preview card, designed to highlight key content elements such as publication dates and author info. Built with Flexbox for a responsive and modern presentation.", imageUrl: ""}
+  {
+    id: 1,
+    title: "Social Links Profile",
+    description: "An intuitive profile page designed to centralize your essential social media links in one responsive, easy-to-navigate space. Perfect for enhancing your online presence and boosting engagement.",
+    imageUrl: "https://github.com/jpcardozx/react-portfolio-app/blob/main/src/assets/social-media-links.png?raw=true",
+    linkUrl: "https://jpcardozx.github.io/social-links-profile/"
+  },
+  {
+    id: 2,
+    title: "Product Ad | UI/UX Design",
+    description: "A highly optimized product ad landing page, built with a focus on user engagement and lead conversion. This design combines modern UI/UX principles for a smooth and effective experience.",
+    imageUrl: "https://github.com/jpcardozx/react-portfolio-app/blob/main/src/assets/ad-prvw.png?raw=true",
+    linkUrl: "https://jpcardozx.github.io/product-ad-preview/"
+  },
+  {
+    id: 3,
+    title: "Testimonials Grid | Advanced CSS",
+    description: "A sleek, responsive grid showcasing client testimonials using advanced CSS. This layout enhances credibility and fits seamlessly across all screen sizes, perfect for businesses highlighting social proof.",
+    imageUrl: "https://github.com/jpcardozx/react-portfolio-app/blob/main/src/assets/tgrid.png?raw=true",
+    linkUrl: "https://jpcardozx.github.io/testimonials-grid-section/"
+  },
+  {
+    id: 4,
+    title: "Blog Preview Card | CSS Flexbox",
+    description: "A customizable blog preview card, designed to highlight key content elements such as publication dates and author info. Built with Flexbox for a responsive and modern presentation.",
+    imageUrl: "",
+    linkUrl: "https://jpcardozx.github.io/blog-preview/"
+  }
 ];
+;
 
 const ProjectList: React.FC = () => {
   return (
